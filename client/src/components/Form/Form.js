2e9a7d7dd3b83de3d72688f01186e90bf9534ae7
Karteek <ati.karteek99@gmail.com>
2022-10-19 15:05:28 +0530

    form validation -- not working

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,61 +1,192 @@
-import React from "react";
-import { useState } from 'react';
-import { Container, Paper, Box, Typography, Grid, TextField, Button} from '@material-ui/core';
-import useStyles from './style';
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import {
+  Container,
+  Paper,
+  Box,
+  Typography,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import useStyles from "./style";
 import IconButton from "@material-ui/core/IconButton";
-import RemoveButton from '@material-ui/icons/Remove';
-import FileBase from 'react-file-base64';
- 
+import RemoveButton from "@material-ui/icons/Remove";
+import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
+
 const Form = () => {
-    const classes = useStyles();
-    const [postData, setPostData] = useState({
-        title: '', description: '', creator: '', cuisine: '', ingredients: [], tags: [], selectedFile: ''  
-    });
-    const [ingredients, setIngredient] = useState(['']);
+  const classes = useStyles();
+  const [postData, setPostData] = useState({
+    title: "",
+    description: "",
+    creator: "",
+    cuisine: "",
+    ingredients: [],
+    tags: [],
+    selectedFile: "",
+  });
+  const [ingredients, setIngredient] = useState([""]);
+  const [tags, setTags] = useState("");
+  const { register, handleSubmit, control, errors } = useForm();
 
-    const changeIngredient = (index,e) => {
-        const values = [...ingredients];
-        values[index] = e.target.value;
-        setIngredient(values)
-        setPostData({...postData, ingredients:ingredients})
-    }
+  const dispatch = useDispatch();
 
+  const changeIngredient = (index, e) => {
+    const values = [...ingredients];
+    values[index] = e.target.value;
+    setIngredient(values);
+    setPostData({ ...postData, ingredients: ingredients });
+  };
 
-    const addIngredient = (e) => {
-        setIngredient([...ingredients, ''])
-        setPostData({...postData, ingredients:ingredients});
-    }
+  const addIngredient = (e) => {
+    setIngredient([...ingredients, ""]);
+    setPostData({ ...postData, ingredients: ingredients });
+  };
 
-    const deleteIngredients = (index) => {
-        const deleteVal = [...ingredients];
-        deleteVal.splice(index,1);
-        setIngredient(deleteVal);
-    }
+  const deleteIngredients = (index) => {
+    if (index === 0) return;
+    const deleteVal = [...ingredients];
+    deleteVal.splice(index, 1);
+    setIngredient(deleteVal);
+  };
 
-    return( 
-        <Paper> 
-            <form className={`${classes.root} ${classes.form}`} autoComplete="off" noValidate>
-                <Typography>Creating a Recipe</Typography>
-                <TextField name="title" variant="outlined" label="Title" fullWidth onChange={(e) => setPostData({...postData, title : e.target.value})}> </TextField>
-                <TextField name="description" multiline variant="outlined" label="Description" fullWidth onChange={(e) => setPostData({...postData, description : e.target.value})}> </TextField>
-                <TextField name="cuisine"  variant="outlined" label="Cuisine" fullWidth onChange={(e) => setPostData({...postData, cuisine : e.target.value})}> </TextField>
-                {   
-                    ingredients.map((ingredient,index) => (
-                            <div key={index} style={{display : 'inline-flex', width:'100%'}}>
-                                <TextField name="ingredient" variant="outlined" label={index === 0 ? 'Ingredient' : 'Ingredient '+index} fullWidth onChange={(e) => changeIngredient(index,e)}></TextField>   
-                                <IconButton onClick={(e) => {deleteIngredients(index)}}>
-                                    <RemoveButton  />
-                                </IconButton>
-                            </div>
-                    ))
-                    
-                }
-                <div className={classes.buttonSubmit}><Button variant="contained" color="secondary" size="small" onClick={(e) => addIngredient(e)}>Add</Button></div>
-                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}></FileBase></div>
-                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth   >Submit</Button>
-            </form>
-        </Paper>
-    )
-}
+  const addTags = (e) => {
+    setPostData({ ...postData, ingredients: ingredients });
+    setTags(e.target.value);
+  };
+
+  const lastUpdate = () => {
+    const tag = tags.split(",").map((tag, index) => tag.trim());
+    setPostData({ ...postData, tags: tag });
+  };
+
+  const handleSubmits = (data) => {
+    //e.preventDefault();
+    console.log(postData);
+    dispatch(createPost(postData));
+  };
+
+  return (
+    <Paper>
+      <form
+        className={`${classes.root} ${classes.form}`}
+        autoComplete="off"
+        noValidate
+        onSubmit={handleSubmit(handleSubmits)}
+      >
+        <Typography>Creating a Recipe</Typography>
+
+        <Controller
+          control={control}
+          name="title"
+          render={({ field: { onChange, value, ref } }) => (
+            <TextField
+              required
+              variant="outlined"
+              label="Title"
+              fullWidth
+              onChange={(e) => onChange={(e) => {
+                
+              }}
+            ></TextField>
+          )}
+          rules={{ required: true }}
+        />
+
+        <TextField
+          name="description"
+          multiline
+          required
+          variant="outlined"
+          label="Description"
+          fullWidth
+          {...register("description", { required: "Description Required" })}
+          onChange={(e) =>
+            setPostData({ ...postData, description: e.target.value })
+          }
+          error={!!errors?.description}
+          helperText={errors?.description ? errors.description.message : ""}
+        ></TextField>
+        <TextField
+          name="cuisine"
+          variant="outlined"
+          required
+          label="Cuisine"
+          fullWidth
+          {...register("cuisine", { required: "Cuisine Required" })}
+          onChange={(e) => setPostData({ ...postData, cusine: e.target.value })}
+          error={!!errors?.cuisine}
+          helperText={errors?.cuisine ? errors.cuisine.message : ""}
+        ></TextField>
+        {ingredients.map((ingredient, index) => (
+          <div key={index} style={{ display: "inline-flex", width: "100%" }}>
+            <TextField
+              name="ingredient"
+              variant="outlined"
+              required
+              label={index === 0 ? "Ingredients" : "Ingredient " + index}
+              fullWidth
+              {...register("ingredient", { required: "Ingredient Required" })}
+              onChange={(e) => changeIngredient(index, e)}
+              error={!!errors?.ingredient}
+              helperText={errors?.ingredient ? errors.ingredient.message : ""}
+            ></TextField>
+            <IconButton
+              onClick={(e) => {
+                deleteIngredients(index);
+              }}
+            >
+              <RemoveButton />
+            </IconButton>
+          </div>
+        ))}
+        <div className={classes.buttonSubmit}>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="small"
+            onClick={(e) => addIngredient(e)}
+          >
+            Add
+          </Button>
+        </div>
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          onChange={(e) => addTags(e)}
+          {...register("tags", { required: "Tags Required" })}
+          error={!!errors?.tags}
+          helperText={
+            errors?.tags ? errors.tags.message : "Comma Separated Tags"
+          }
+        ></TextField>
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          ></FileBase>
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+          onClick={lastUpdate}
+        >
+          Submit
+        </Button>
+      </form>
+    </Paper>
+  );
+};
 
 export default Form;
